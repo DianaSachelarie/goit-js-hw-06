1d436805dd06c48e38592bca3df43500255cf57d
DianaSachelarie <diana_sachelarie@yahoo.com>
2023-10-23 23:58:54 +0300

    First commit

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelector("#categories");
+const itemsList = categoriesList.querySelectorAll(".item");
+
+console.log(`Number of categories: ${itemsList.length}`);
+
+itemsList.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const categoryElements = item.querySelectorAll("li").length;
+  console.log(`Category: ${categoryTitle}\nElements: ${categoryElements}`);
+});
